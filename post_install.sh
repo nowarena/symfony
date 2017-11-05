e3c43a373629ceb966ef89c0ae26f3e9c58f3ba3
matt <mbbyrnes@gmail.com>
2017-11-05 10:45:17 -0800

    first push

---
+++ post_install.sh
@@ -0,0 +1,8 @@
+mkdir /var/www/symfony/var/cache/prod
+chmod 777 /var/www/symfony/var/cache/prod
+
+mkdir /var/www/symfony/var/logs
+chmod 777 /var/www/symfony/var/logs
+
+mkdir /var/www/symfony/var/sessions/prod
+chmod 777 /var/www/symfony/var/sessions/prod
